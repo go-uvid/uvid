66cebd75a59b6c02964ae1de056088aa749d0b97
luvsic3 <youruichao@gmail.com>
2023-08-05 15:20:33 +0800

    fix: tsc error

--- js/packages/dash/src/lib/text.ts
+++ js/packages/dash/src/lib/text.ts
@@ -1,8 +1,11 @@
 const offscreen = new OffscreenCanvas(screen.width, screen.height);
-const context = offscreen.getContext('2d');
+const context = offscreen.getContext(
+	'2d',
+) as unknown as CanvasRenderingContext2D;
 
 export function measureTextWidth(text: string, font = '') {
 	context?.save();
-	context!.font = font;
+	context.font = font;
+	context?.restore();
 	return context?.measureText(text).width;
 }
